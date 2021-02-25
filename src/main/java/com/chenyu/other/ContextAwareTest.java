338e7cc1b984ac58e8fdee3a6036a52c21688d9f
youfangqiao <772009202@qq.com>
2021-02-25 21:28:14 +0800

    ..

--- src/main/java/com/chenyu/other/ContextAwareTest.java
+++ src/main/java/com/chenyu/other/ContextAwareTest.java
@@ -14,6 +14,7 @@ public class ContextAwareTest implements ApplicationContextAware {
   /** 全局的上下文 */
   public static ApplicationContext context;
 
+
   public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
     ContextAwareTest.context = applicationContext;
   }
